4d0c386434d369447362e911e0ac8777c81b6946
basalamahalam <basalamahalam@upi.edu>
2024-08-02 15:15:24 +0700

    commit pertama

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,59 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarComponents from "./components/NavbarComponents";
 import ListCategories from "./components/ListCategories";
+import Menus from "./components/Menus";
 import Hasil from "./components/Hasil";
 import { Col, Container, Row } from "react-bootstrap";
+import React, { Component } from "react";
+import { API_URL } from "./utils/constants";
+import axios from "axios";
 
-function App() {
-  return (
-    <>
-      <NavbarComponents />
-      <div className="mt-3">
-        <Container fluid>
-          <Row>
-            <ListCategories />
-            <Col>
-              <h4>
-                <strong>Daftar Produk</strong>
-              </h4>
-              <hr />
-            </Col>
-            <Hasil />
-          </Row>
-        </Container>
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menus: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + "products")
+      .then((res) => {
+        // console.log(response);
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }
+  render() {
+    const { menus } = this.state;
+    return (
+      <div className="App">
+        <NavbarComponents />
+        <div className="mt-3">
+          <Container fluid>
+            <Row>
+              <ListCategories />
+              <Col>
+                <h4>
+                  <strong>Daftar Produk</strong>
+                </h4>
+                <hr />
+                <Row>
+                  {menus &&
+                    menus.map((menu) => <Menus key={menu.id} menu={menu} />)}
+                </Row>
+              </Col>
+              <Hasil />
+            </Row>
+          </Container>
+        </div>
       </div>
-    </>
-  );
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Menus.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Col, Card } from "react-bootstrap";
+import { numberWithComas } from "../utils/utils";
+
+const Menus = ({ menu }) => {
+  return (
+    <Col md={4} xs={6} className="mb-4">
+      <Card className="shadow">
+        <Card.Img
+          variant="top"
+          src={"images/" + menu.category.nama.toLowerCase() + "/" + menu.gambar}
+        />
+        <Card.Body>
+          <Card.Title>
+            {menu.nama} <strong>({menu.kode})</strong>
+          </Card.Title>
+          <Card.Text>Rp. {numberWithComas(menu.harga)}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
+export default Menus;
