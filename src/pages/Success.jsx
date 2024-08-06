d8abc38f2cbce22e40182d0d1692d7575ab4098a
basalamahalam <basalamahalam@upi.edu>
2024-08-06 16:47:31 +0700

    fix assets & sukses

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default class Success extends Component {
   render() {
     return (
       <div className="mt-4 text-center">
+        <Image src="images/sukses.png" width="500" alt="sukses" />
         <h2>Sukses Pesan</h2>
         <p>Terimakasih Sudah Memesan</p>
         <Button variant="primary" as={Link} to="/">
