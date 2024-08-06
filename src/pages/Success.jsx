839ab467b9bc3486a778873763579304f27aadc8
basalamahalam <basalamahalam@upi.edu>
2024-08-06 16:01:01 +0700

    Halaman Sukses

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default class Success extends Component {
   render() {
-    return <div>Success</div>;
+    return (
+      <div className="mt-4 text-center">
+        <h2>Sukses Pesan</h2>
+        <p>Terimakasih Sudah Memesan</p>
+        <Button variant="primary" as={Link} to="/">
+          Kembali
+        </Button>
+      </div>
+    );
   }
 }
