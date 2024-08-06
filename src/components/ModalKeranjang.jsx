4ffb1dda1efaa216b35ccd899509c5bd4c22b5c9
basalamahalam <basalamahalam@upi.edu>
2024-08-07 00:29:05 +0700

    fix modal style

--- src/components/ModalKeranjang.jsx
+++ src/components/ModalKeranjang.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { Button, Modal, Form } from "react-bootstrap";
 import { numberWithComas } from "../utils/utils";
+import { faMinus, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const ModalKeranjang = ({ showModal, handleClose, keranjangDetail }) => {
+const ModalKeranjang = ({
+  showModal,
+  handleClose,
+  keranjangDetail,
+  jumlah,
+  keterangan,
+}) => {
   // Cek apakah keranjangDetail dan keranjangDetail.product ada
   const hasKeranjangDetail = keranjangDetail && keranjangDetail.product;
 
@@ -27,31 +35,48 @@ const ModalKeranjang = ({ showModal, handleClose, keranjangDetail }) => {
         {hasKeranjangDetail ? (
           <Form>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="name@example.com"
-                autoFocus
-              />
+              <Form.Label>Total Harga</Form.Label>
+              <p>
+                <strong>
+                  Rp. {numberWithComas(keranjangDetail.total_harga)}
+                </strong>
+              </p>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Jumlah :</Form.Label>
+              <br />
+              <Button variant="primary" size="sm" className="me-2">
+                <FontAwesomeIcon icon={faPlus} />
+              </Button>
+              <strong>{jumlah}</strong>
+              <Button variant="primary" size="sm" className="ms-2">
+                <FontAwesomeIcon icon={faMinus} />
+              </Button>
             </Form.Group>
             <Form.Group
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
             >
-              <Form.Label>Example textarea</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Label>Keterangan :</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                name="keterangan"
+                placeholder="Contoh: Pedas, Nasi Setengah, dst"
+                value={keterangan}
+              />
             </Form.Group>
+            <Button variant="primary" type="submit">
+              Simpan
+            </Button>
           </Form>
         ) : (
           "kosong"
         )}
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-        <Button variant="primary" onClick={handleClose}>
-          Save Changes
+        <Button variant="danger" onClick={handleClose}>
+          <FontAwesomeIcon icon={faTrash} /> Hapus Pesanan
         </Button>
       </Modal.Footer>
     </Modal>
